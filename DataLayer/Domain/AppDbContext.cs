62de0436143c6cd754a08651ac0a2e13fecb339d
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2022-10-24 22:17:53 +0700

    Define model and db context

---
+++ DataLayer/Domain/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Domain
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<AppRole> Roles { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookAuthor> BookAuthorSet { get; set; }
+
+    }
+}
---
+++ DataLayer/Domain/AppEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Domain
+{
+    public enum BookType
+    {
+        Unknown,
+        Type1,
+        Type2,
+    }
+
+    public enum City
+    {
+        Unknown,
+        City1,
+        City2,
+    }
+
+    public enum State
+    {
+        Unknown,
+        State1,
+        State2,
+    }
+
+    public enum Country
+    {
+        Unknown,
+        Country1,
+        Country2,
+    }
+}
